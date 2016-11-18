ec5672aa3e1a2e7486941ddc6f8e66b714d4d2e1
agauli <apps.ashu@gmail.com>
2016-11-17 19:50:44 -0600

    Update reducer.py

--- Desktop/Yelp-Data-Challenge/Mapreduce/reducer.py
+++ Desktop/Yelp-Data-Challenge/Mapreduce/reducer.py
@@ -1,18 +1,39 @@
 #!/usr/bin/python
 import sys
-attire = None
-Count = [0]*24
-for line in sys.stdin:
-    data_mapped = line.strip().split("\t")
-    if len(data_mapped) != 2:
-# Something has gone wrong. Skip this line.
-        continue
-    thisCity, thisattire = data_mapped
-    if oldCity and oldCity != thisCity:
-        max_value = max(thisattire)
-        max_index = thisattire.index(max_value)
-        print oldCity, max_index
-        oldCity = thisCity
-    Count = [0]*24
-    oldCity = thisCity
-    Count[int(attire)] += 1
+import csv
+
+def print_dict(dict):
+    count_str = '\t'
+    a = ['', 'beer_and_wine','none', 'full_bar' ]
+    for e in a:
+        if e in dict:
+            count_str = count_str + str(dict[e])
+        else:
+            count_str = count_str + '0'
+        count_str = count_str + "\t"
+    return count_str
+
+def reducer():
+    reader = csv.reader(sys.stdin, delimiter='\t')
+    writer = csv.writer(sys.stdout, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+    prevCity = None
+    f = None
+    alchol_count = {}
+    
+    for line in reader:
+        if(len(line) == 2):
+            currentCity, alchol = line
+        if prevCity and prevCity != currentCity:
+            print prevCity, alchol_count.values()
+            alchol_count = {}
+        prevCity = currentCity
+        if alchol in alchol_count:
+                alchol_count[alchol] = alchol_count[alchol]  + 1
+        else:
+                alchol_count[alchol] = 1
+    if prevCity != None:
+        print prevCity, print_dict(alchol_count)
+if __name__ == "__main__":
+    reducer()
+
+
